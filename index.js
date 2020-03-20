add73cf8234870d7a11015e1bd2e1d67ee5a8661
Vasu Patel <Vasu.Patel@ibm.com>
2020-03-20 17:18:04 -0300

    used async/await and resolved made code more readable and reduced the api response time

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
 let got = require('got');
 
-app.get('/:id', (req, res) => {
-    let temp;
+app.get('/:id',async (req, res) => {
 
-    let url = `https://swapi.co/api/people/${req.params.id}`;
-
-    getData(url)
-        .then(function (response) {
-            temp = response.body;
-
-            getData(temp.homeworld)
-                .then((r_tempHomeWorld) => {
-                    temp.homeworld = r_tempHomeWorld.body;
-
-                    getData(temp.species[0])
-                        .then((r_tempSpices) => {
-
-                            temp.species = r_tempSpices.body;
-                           
-
-                        }).then(()=>{
-                            let tempFilms = [];
-
-                            //maping promises with every film fetch
-                            var actions=temp.films.map(filmGetter);
-                            var results = Promise.all(actions);
-                            
-                            results.then(data=>{
-                                data.forEach(element => {
-                                    tempFilms.push(element.body)
-                                });  
-                                temp.films=tempFilms;
-                                res.send(temp)
-                            })
-                            
-                        });
-                });
-        })
-
-    // getPersonByUrl(url).then((result)=>{
-    //     console.log(result)
-    //     res.send(result);
-    // })
-
-
+    const url = `https://swapi.co/api/people/${req.params.id}`;
+    try{
+         const character = await getData(url);
+         const homeWorld = await getData(character.body.homeworld);
+         const species = await getData(character.body.species[0]);
+         const films = await getFilms(character.body.films);
+         res.status(200).send({...character.body,homeworld:homeWorld.body,species:species.body,films:films})
+        
+    }catch(e){
+        res.status(404).send({"error":"something went wrong"})
+    }
 });
 
 
@@ -88,14 +56,30 @@ const getPersonByUrl = (url) =>{
 
 //making promise to fetch data from the API
 const getData = url => {
-    return got(url, { json: true })      
+    return new Promise((resolve,reject)=>{
+        try{
+            resolve(got(url, { json: true }));     
+        }catch(e){
+           reject(e);
+        }
+    }) 
 };
 
-//making promise function to map all films 
-const filmGetter =  film => {
-    return new Promise(resolve=> resolve(getData(film)));
-}
 
+const getFilms = async (films)=>{
+    const actions= await Promise.all(films.map(getData));
+    return new Promise((resolve,reject)=>{
+        try{
+            let tempFilms = [];
+            actions.forEach(element => {
+                    tempFilms.push(element.body)
+            });  
+            resolve(tempFilms);     
+        }catch(e){
+           reject(e);
+        }
+    }) 
+}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
