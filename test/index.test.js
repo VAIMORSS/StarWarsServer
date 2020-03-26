62a4c23a4c9df5d575b3e2e11c354d3a62c75046
Vasu Patel <Vasu.Patel@ibm.com>
2020-03-25 21:10:47 -0300

    new tests implemented

--- test/index.test.js
+++ test/index.test.js
@@ -2,9 +2,14 @@ const request = require("supertest");
 const app = require('../app.js');
 const testPerson = require('./testPerson.json')
 
+/**
+ * In case of need of got module's functionality we can use this newGot!
+ */
+//const newGot = jest.requireActual('got');
+
 jest.mock('got');
 
-describe('App, accepts an id of character and sends back the detrails of the character', () => {
+describe('App module test for person with id 1!', () => {
     let response;
 
     beforeEach(async () => {
@@ -16,26 +21,42 @@ describe('App, accepts an id of character and sends back the detrails of the cha
         done();
     });
 
-    test("name of the person fetrched is same", async (done) => {
-        expect(response.body.name).toBe('Luke Skywalker');
-        done();
-    });
-
-    test("length of the film should be same", async (done) => {
-        expect(response.body.films.length).toBe(5);
-        done();
-    });
-
-    test("length of the starship should be same", async (done) => {
-        expect(response.body.starships.length).toBe(2);
-        done();
-    });
 
     test("the whole object should match with local json", async (done) => {
         expect(response.body).toEqual(testPerson);
         done();
     });
 });
+
+
+describe('App module test for person with id 2!', () => {
+    let response;
+
+    beforeEach(async () => {
+        response = await request(app).get("/2");
+    });
+
+    test("response status is okay", async (done) => {
+        expect(response.status).toBe(200);
+        done();
+    });
+
+    test("name of the person fetrched is same", async (done) => {
+        expect(response.body.name).toBe('C-3PO');
+        done();
+    });
+
+    test("length of the film should be same", async (done) => {
+        expect(response.body.films.length).toBe(6);
+        done();
+    });
+
+    test("length of the starship should be same", async (done) => {
+        expect(response.body.starships.length).toBe(0);
+        done();
+    });
+});
+
 
 
 
